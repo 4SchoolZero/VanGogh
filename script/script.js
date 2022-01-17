21a62c0614726123e7c8d1f7fbe0d7ea1e93e535
Tijmen <34179@ma-web.nl>
2022-01-17 09:48:52 +0100

    added history, img slider

--- script/script.js
+++ script/script.js
@@ -2,3 +2,29 @@ const text = document.getElementById('txt');
 function change(){
     text.innerHTML = "Your life would be very empty if you didn't regret anything.";
 }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
